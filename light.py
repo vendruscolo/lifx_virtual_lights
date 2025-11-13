ea733284433ba3573733735c565963584ecc2b4e
Alessandro Vendruscolo <alessandro.vendruscolo@gmail.com>
2025-11-13 08:16:00 +0100

    Photon ranges are 0-65535

--- light.py
+++ light.py
@@ -333,16 +333,16 @@ def hue_photons_to_ha(value):
     return int(round(value / 65535)) * 360
 
 def hue_ha_to_photons(value):
-    return value / 360
+    return value / 360 * 65535
 
 def brightness_photons_to_ha(value):
     return int(round(value / 65535)) * 255
 
 def brightness_ha_to_photons(value):
-    return value / 255
+    return value / 255 * 65535
 
 def saturation_photons_to_ha(value):
     return int(round(value / 65535) * 100)
 
 def saturation_ha_to_photons(value):
-    return value / 100
+    return value / 100 * 65535
